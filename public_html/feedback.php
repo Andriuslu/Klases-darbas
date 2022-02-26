96f8729e9f958420ae0d6487b40a01e1e99cde03
Andrius Luzan <andriuslu@gmail.com>
2022-02-27 00:31:21 +0200

    started new DB for Atsiliepimai (not working)

---
+++ public_html/feedback.php
@@ -0,0 +1,30 @@
+<?php
+
+$name = $_POST['name'];
+echo($name);
+$feedback = $_POST['feedback'];
+
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "KlasesDarbas";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT id, first_name, last_name FROM persons";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
