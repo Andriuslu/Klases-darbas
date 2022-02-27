86dcc474d61c7ecd96325557682e8692647d2355
Andrius Luzan <andriuslu@gmail.com>
2022-02-28 00:53:22 +0200

    after changed localhost to db, the change took effect. server and database working. added header. updated connection code.

--- public_html/feedback.php
+++ public_html/feedback.php
@@ -1,13 +1,47 @@
+<!Doctype html>
+<html lang="lt">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Automobilio valymas</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/Aruno_svara_dizainas.css">
+    <link rel="stylesheet" href="css/Aruno_svara_layout.css">
+</head>
+
+<body>
+
+<div>
+    <div class="a header">
+        <div class="img_valymas">
+            <img src="img/valymas_logo.jpeg" alt="LOGO" style="width: 148px; height: 148px;">
+        </div>
+        <div class="arunas_svara">
+            <h3 class="text">Arūno švara<br></h3>
+            <p class="text">Cheminis valymas</p>
+            <a href="index.html"><button class="btn">Paslaugos</button></a>
+            <a href="Automobilio_valymas.html"><button class="btn">Automobilio valymas</button></a>
+            <a href="Baldu_valymas.html"><button class="btn">Baldu valymas</button></a>
+            <a href="Irangos_nuoma.html"><button class="btn">Įrangos nuoma</button></a>
+            <a href="Aplinkos_tvarkymas.html"><button class="btn">Aplinkos tvarkymas</button></a>
+            <a href="Aruno_svara_kontaktai.html"><button class="btn">Kontaktai</button></a>
+            <a href="Aruno_svara_galerija.html"><button class="btn">Galerija</button></a>
+            <a href="feedba.php"><button class="btn">Atsiliepimai</button></a>
+        </div>
+        <div class="contact_links">
+            <a href="https://www.facebook.com/arunosvara" target="_blank"><img class="fb_img" src="img/FBB.png" width="512" height="512" alt="Facebook"/></a>
+        </div>
+    </div>
+</div>
+<h4>Mums svarbi Jūsų nuomonė. Dėkojame už atsiliepimą!</h4>
+</body>
+
 <?php
 
-$name = $_POST['name'];
-echo($name);
-$feedback = $_POST['feedback'];
-
-$servername = "localhost";
-$username = "username";
-$password = "password";
-$dbname = "KlasesDarbas";
+$servername = "db";
+$username = "devuser";
+$password = "devpass";
+$dbname = "kcs_db";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -15,16 +49,11 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+$name = $_POST['name'];
 
-$sql = "SELECT id, first_name, last_name FROM persons";
+$feedback = $_POST['feedback'];
+$sql = "INSERT INTO feedbacks (Name, feedback) VALUES ('$name','$feedback')";
 $result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
-    }
-} else {
-    echo "0 results";
-}
 $conn->close();
+
