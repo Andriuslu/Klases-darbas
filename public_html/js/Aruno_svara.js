e2b8462581ab4b2a36a790b63a832cb59a686d58
Andrius Luzan <andriuslu@gmail.com>
2022-03-01 18:31:07 +0200

    updated

--- public_html/js/Aruno_svara.js
+++ public_html/js/Aruno_svara.js
@@ -1,14 +1,14 @@
-let pictureNumber = 0;
+let pictureNumber = 1;
 let pictureName = "img/AR_";
 $("#left").click(function () {
     if (pictureNumber > 1) {
         pictureNumber--;
-        $(".resize").attr("src", "img/AR_" + pictureNumber + ".jpeg");
+        $(".resize").attr("src", pictureName + pictureNumber + ".jpeg");
     }
 })
 $("#right").click(function () {
     if (pictureNumber < 13) {
         pictureNumber++;
-        $(".resize").attr("src", "img/AR_" + pictureNumber + ".jpeg");
+        $(".resize").attr("src", pictureName + pictureNumber + ".jpeg");
     }
 })
